e5a202f2ba33a4cb78d67ea79e012002ce624a77
Joey Palchak <joey.palchak@gmail.com>
2023-08-11 14:33:13 -0700

    Expand branch options for survey results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 // BUSINESS LOGIC
 
-
 // UI LOGIC
 
 // reset() will hide errors that have been resolved,
@@ -14,15 +13,12 @@ function reset() {
 // showResult() will un-hide result text block, and modify
 // the survey's answer according to the language passed through as an argument
 function showResult(language) {
-  // create variable for object of result text block, initially hidden
   const text = document.getElementById('results');
-  // create variable for object of survey answer text
   const answer = document.getElementById('answer');
 
-  // modify answer text to suggested language
   answer.innerText = language;
-  // show the results of the survey
   text.removeAttribute('class', 'hidden');
+  text.scrollIntoView();
 }
 
 // checkErrors() will check for multiple error scenarios in the form.
@@ -30,9 +26,8 @@ function showResult(language) {
 // as well as return a boolean value of true.
 // if there are no errors, it will return a boolean value of false
 function checkErrors(career, language){
-  let error;
-  if (career === '0' || language === '0') {
-    error = true;
+  let error = (career === '0' || language === '0');
+  if (error) {
     if (career === '0' && language == '0') { 
       document.getElementById('error-interest').removeAttribute('class', 'hidden');
       document.getElementById('error-career').removeAttribute('class', 'hidden');
@@ -41,34 +36,46 @@ function checkErrors(career, language){
     } else {
       document.getElementById('error-interest').removeAttribute('class', 'hidden');
     }
-  } else {
-    error = false;
   }
   return error;
 }
 
+function suggestLanguage(puzzle, os, career) {
+  if (puzzle === 'no') {
+    showResult("If you don't like puzzles, maybe programming isn't for you...");
+  } else if (os != 'mac' && (career === 'app' || career === 'game')) {
+    showResult('C++');
+  } else if (os === 'mac' && career === 'app'){
+    showResult('Swift');
+  } else if (os === 'mac' && career === 'game') {
+    showResult('Java');
+  } else if (career === 'cyber') {
+    showResult('Python');
+  } else if (career === 'web') {
+    showResult('JavaScript')
+  } 
+}
+
 // handleSubmitEvent() creates an event handler for the form submission
 function handleSubmitEvent(e) {
-  // prevent page refresh
   e.preventDefault();
   // if re-submitting, reset results and error messages
   reset();
-  // grab answers to each survey question
-  const experience = document.querySelector("input[name='experience']:checked").value;
+
   const career = document.getElementById("career").value;
   const puzzle = document.querySelector("input[name='puzzle']:checked").value;
+  const os = document.querySelector("input[name='os']:checked").value;
   const language = document.getElementById("select-interest").value;
  
+  // check for errors
   const error = checkErrors(career, language);
-  // here we'll check survey results
+  // branching logic to decide survey results
   if (language != 'help' && !error){  
     // if there's no error, and the user has a language they're interested in
     // just show the language they chose
     showResult(language);
   } else if (language === 'help' && !error) { 
-    // if there's no error, and the user doesn't pick an initial language
-    // we'll go through some logic to choose one based on the form input
-    showResult('this is where we branch');
+    suggestLanguage(puzzle, os, career);
   }
 }
 
