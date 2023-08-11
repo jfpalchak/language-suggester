a9c2c4d786198229c62a948949d8db51f9fda271
Joey Palchak <joey.palchak@gmail.com>
2023-08-11 11:11:57 -0700

    Organize JavaScript functions and add comments for readability.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,30 @@
 
 // UI LOGIC
 
-// when called, reset() will hide errors that have been resolved,
+// reset() will hide errors that have been resolved,
 // as well as remove the previous survey result if a new error is returned
 function reset() {
   document.getElementById('error').setAttribute('class', 'hidden');
   document.getElementById('results').setAttribute('class', 'hidden')
+}
+
+// showResult() will un-hide result text block, and modify
+// the survey's answer according to the language passed through as an argument
+function showResult(language) {
+  // create variable for object of result text block, initially hidden
+  const text = document.getElementById('results');
+  // create variable for object of survey answer text
+  const answer = document.getElementById('answer');
+
+  // modify answer text to suggested language
+  answer.innerText = language;
+  // show the results of the survey
+  text.removeAttribute('class', 'hidden');
+}
+
+// when called, showError() will update the form with an error message
+function showError() {
+  document.getElementById('error').removeAttribute('class', 'hidden');
 }
 
 // load page resources before running JS functions
@@ -22,17 +41,16 @@ function handleSubmitEvent(e) {
   const career = document.getElementById("career").value;
   const language = document.getElementById("select-interest").value;
 
-  // create variable for object of survey result text, initially hidden
-  // let results = document.getElementById('answer').innerText;
+  // const text = document.getElementById('results');
+  // const answer = document.getElementById('answer');
 
   // here we'll check survey results
-  if (language === '0'){
-    document.getElementById('error').removeAttribute('class', 'hidden');
+  if (language === '0'){ 
+    showError();
   // } else if (language === 'help') {
 
-  } else {
-    document.getElementById('answer').innerText = language;
-    document.getElementById('results').removeAttribute('class', 'hidden');
+  } else { // if a specific language was selected, we'll suggest that language
+    showResult(language);
   }
 }
 
