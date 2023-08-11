e862bb891f1978545f760012e15023e5bca262f4
Joey Palchak <joey.palchak@gmail.com>
2023-08-11 10:30:38 -0700

    Add initial branching, error message, and reset function.

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,37 @@
 
 // UI LOGIC
 
+// when called, reset() will hide errors that have been resolved,
+// as well as remove the previous survey result if a new error is returned
+function reset() {
+  document.getElementById('error').setAttribute('class', 'hidden');
+  document.getElementById('results').setAttribute('class', 'hidden')
+}
+
 // load page resources before running JS functions
 function handleSubmitEvent(e) {
   // prevent page refresh
   e.preventDefault();
+  // if re-submitting, reset error messages
+  reset();
 
   // grab answers to each survey question
   const experience = document.querySelector("input[name='experience']:checked").value;
   const career = document.getElementById("career").value;
-  const interest = document.getElementById("select-interest").value;
+  const language = document.getElementById("select-interest").value;
+
+  // create variable for object of survey result text, initially hidden
+  // let results = document.getElementById('answer').innerText;
 
   // here we'll check survey results
-  let result;
+  if (language === '0'){
+    document.getElementById('error').removeAttribute('class', 'hidden');
+  // } else if (language === 'help') {
 
+  } else {
+    document.getElementById('answer').innerText = language;
+    document.getElementById('results').removeAttribute('class', 'hidden');
+  }
 }
 
 function handleForm() {
