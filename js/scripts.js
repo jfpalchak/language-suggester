969565c1af0765292461be9ad814077e64b966c1
Joey Palchak <joey.palchak@gmail.com>
2023-08-11 09:04:37 -0700

    Create initial JS functions for event handling.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// load page resources before running JS functions
+window.addEventListener("load", function() {
+  const form = document.getElementById("form");
+
+  // handle 'submit' button event
+  form.addEventListener("submit", function(e){
+    e.preventDefault(); 
+    
+  });
+
+});
