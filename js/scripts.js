544705b9f2bd9875fecefa5398fc0c3eb0c99621
Joey Palchak <joey.palchak@gmail.com>
2023-08-11 09:44:28 -0700

    Add initial function organization and create variables for user inpuit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 // load page resources before running JS functions
-window.addEventListener("load", function() {
+function handleSubmitEvent(e) {
+  // prevent page refresh
+  e.preventDefault();
+
+  // grab answers to each survey question
+  const experience = document.querySelector("input[name='experience']:checked").value;
+  const career = document.getElementById("career").value;
+  const interest = document.getElementById("select-interest").value;
+
+  // here we'll check survey results
+
+}
+
+function handleForm() {
   const form = document.getElementById("form");
+  form.addEventListener('submit', handleSubmitEvent);
+}
 
-  // handle 'submit' button event
-  form.addEventListener("submit", function(e){
-    e.preventDefault(); 
-    
-  });
-
-});
+window.addEventListener("load", handleForm);
